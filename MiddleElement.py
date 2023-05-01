eeb11342baaab95f3887ce97df96b7baac321aeb
MikalJohnson <Johnsonmikal24@gmail.com>
2023-04-30 22:17:21 -0700

    Finished all task

--- MiddleElement.py
+++ MiddleElement.py
@@ -1,3 +1,12 @@
-arr = [1, 3, 5, 7, 9, 10]
+array = [1, 3, 5, 7, 9, 10]
+
 
 def middleelement(arr):
+    halfway = len(arr) // 2
+    print (arr[halfway])
+    if len(arr) % 2 != 0:
+        print(arr[halfway])
+    else:
+        print(arr[halfway - 1], arr[halfway])
+
+middleelement(array)
